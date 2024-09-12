3d3cfd05af14e1e2e6c7b0a32928d1eb5f9b4e02
R-kjim <rkayjim@gmail.com>
2024-09-12 12:06:45 +0300

    class methods and sql database operations

--- lib/debug.py
+++ lib/debug.py
@@ -4,6 +4,29 @@
 from __init__ import CONN, CURSOR
 from department import Department
 
-import ipdb
+# import ipdb
 
-ipdb.set_trace()
+# ipdb.set_trace()
+
+Department.drop_table()
+# print(CURSOR.execute("PRAGMA table_info(departments)").fetchall())
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+# print(payroll)
+# payroll.save()  # Persist to db, assign object id attribute
+payroll.delete()
+print(payroll) 
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+# print(hr) 
+# hr.save()  # Persist to db, assign object id attribute
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update() #update the database
+print(hr)
+
+
+
+
+# print(CURSOR.execute("PRAGMA table_info(departments)").fetchall())
